1e706c2c57f5f0708e86b564e6885151ca6e2008
Leonardo Vogel <leeo.vog13@gmail.com>
2022-04-07 15:49:02 -0300

    styles: add logo trybe

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,9 @@ export function Header() {
   return (
     <Container>
       <Content>
-        <p>logo</p>
+        <img src="https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9ca4630bc5747_Logo.svg" alt="logo trybe" />
         <h1>Try Words</h1>
-        <button type="button">Button</button>
+        <button type="button" disabled>Config</button>
       </Content>
     </Container>
   );
