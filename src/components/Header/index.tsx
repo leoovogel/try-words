fb7c48c6c9062b65fafa6fb0bbd6c8321d9f9692
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-11 07:04:21 -0300

    style: change header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,8 +8,10 @@ export function Header({ openHowToPlayModal }: HowToPlayModalProps) {
   return (
     <Container>
       <Content>
-        <img src="https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9ca4630bc5747_Logo.svg" alt="logo trybe" />
-        <h1>Try Words</h1>
+        <h1>
+          <span>Try</span>
+          Words
+        </h1>
         <button type="button" onClick={openHowToPlayModal}>Como jogar?</button>
       </Content>
     </Container>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-  background: gray;
+  background: #303030;
+  max-height: 80px;
 `;
 
 export const Content = styled.div`
@@ -13,15 +14,33 @@ export const Content = styled.div`
   align-items: center;
   justify-content: space-between;
 
-  button {
-    border: none;
-    background: transparent;
+  h1 {
+    font-size: 3rem;
+    font-weight: bold;
     color: #fff;
-    font-size: 1.8rem;
-    cursor: pointer;
-    font-weight: 700;
-    margin: 0;
-    padding: 0;
+    text-decoration: none;
+    text-transform: uppercase;
+    letter-spacing: 0.4rem;
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
 
+    span {
+      font-size: 2rem;
+      color: #309E76;
+    }
+  }
+
+  button {
+    background: #309E76;
+    border: 0;
+    border-radius: 0.5rem;
+    padding: 0.5rem 1rem;
+    color: #fff;
+    font-size: 1.2rem;
+
+    transition: filter 0.1s;
+    
+    &:hover {
+      filter: brightness(1.2);
+    }
   }
 `;
